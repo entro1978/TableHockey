ea7e371f96cc45c2eba6147c160763e672e9cc73
entro1978 <entro@kth.se>
2013-08-08 16:47:20 +0200

    default

---
+++ TableHockey1/Default.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TableHockey
+{
+    public partial class Register : PageUtility
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
